435acf74d4c14f153c8fed50cbd06f3782602d85
erudite-chirag <161641443+erudite-chirag@users.noreply.github.com>
2024-07-08 11:28:59 -0400

    Day 8 Done

---
+++ QuestionService/src/main/java/com/question/controllers/QuestionController.java
@@ -0,0 +1,44 @@
+package com.question.controllers;
+
+import com.question.enitities.Question;
+import com.question.services.QuestionService;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    //create
+    @PostMapping
+    public Question create(@RequestBody Question question){
+        return questionService.create(question);
+    }
+
+    //get all
+    @GetMapping
+    public List<Question> getAll(){
+        return questionService.get();
+    }
+
+    //get one
+    @GetMapping("/{questionId}")
+    public  Question getOne(@PathVariable Long questionId){
+        return  questionService.getOne(questionId);
+    }
+
+    //get all question of specific quiz
+
+    @GetMapping("/quiz/{quizId}")
+    public  List<Question> getQuestionsOfQuiz(@PathVariable Long quizId){
+        return questionService.getQuestionsOfQuiz(quizId);
+    }
+}
